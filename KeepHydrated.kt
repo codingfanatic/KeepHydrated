8418ffa2ae5c2897d5fafe536a84931becb9eb5a
codingfanatic <lclarkerichard@yahoo.com>
2021-02-24 01:53:20 -0500

    Added test code for converting decimals to Integers Used toInt() method to convert hours cycled into Integers

---
+++ KeepHydrated.kt
@@ -0,0 +1,16 @@
+/*
+Prompt for time
+Round down to whole
+If even, divide by two for liters
+Else, subtract one first, then divide by two for liters
+Display the liters of water drank.
+*/
+
+fun main() {
+    val hoursCycled = 1.99
+    val oop = hoursCycled.toInt()
+
+    print(hoursCycled.toInt())
+
+
+}
